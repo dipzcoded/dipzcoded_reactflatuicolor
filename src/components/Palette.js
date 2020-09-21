e66aec02ea6d08484df34a1a2b915627a84b7117
hp <ithub>
2020-09-21 01:57:01 +0100

    added palette

---
+++ src/components/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Palette = () => {
+    return (
+        <div className="Palette">
+            {/* Navbar Component */}
+            <div className="Palette-colors">
+                {/* Colorbox Component */}
+            </div>
+            {/* Footer Component */}
+        </div>
+    )
+}
+
+export default Palette
