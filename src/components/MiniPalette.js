f3e6ccf70cb2c7ff6022da8211c6cb661e749bbe
dipzcoded <dipoakerele@gmail.com>
2020-11-12 20:43:22 +0100

    done rough styling the palettelist and minipalette component

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -2,30 +2,70 @@ import React, { Fragment } from "react";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal",
-    "& h1": {
-      color: "#ffffff",
+  root: {
+    backgroundColor: "white",
+    border: "1px solid #333",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
     },
   },
-  secondary: {
-    backgroundColor: "pink",
-    "& h1": {
-      color: "yellow",
-    },
+  color: {
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "#000",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emojis: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem",
+  },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    position: "relative",
+    margin: "0 auto",
+    marginBottom: "-4.5px",
   },
 };
 
-const MiniPalette = ({ classes: { main, secondary } }) => {
+const MiniPalette = ({
+  classes: { root, color, title, emojis, miniColor },
+  palette: { paletteName, emoji, colors },
+}) => {
+  const miniColorsBoxes = colors.map((color) => (
+    <div
+      className={miniColor}
+      style={{
+        backgroundColor: color.color,
+      }}
+      key={color.name}
+    />
+  ));
+
   return (
     <Fragment>
-      <div className={main}>
-        <h1>Mini Palette</h1>
-        <section className={secondary}>
-          lorddddddddddddddddddddd
-          <h1>Damn it working!</h1>
-        </section>
+      <div className={root}>
+        <div className={color}>{miniColorsBoxes}</div>
+        <h5 className={title}>
+          {" "}
+          {paletteName} <span>{emoji}</span>
+        </h5>
       </div>
     </Fragment>
   );
--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -2,18 +2,54 @@ import React from "react";
 import seedPalette from "../seed";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/core/styles";
 
-const PaletteList = () => {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+  },
+  paletteGrid: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
+
+const PaletteList = ({ classes: { root, container, nav, paletteGrid } }) => {
   return (
-    <div>
-      <MiniPalette />
-      {seedPalette.map((palette) => (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
-      ))}
+    <div className={root}>
+      <div className={container}>
+        <nav className={nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={paletteGrid}>
+          {seedPalette.map((palette) => (
+            <MiniPalette palette={palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
