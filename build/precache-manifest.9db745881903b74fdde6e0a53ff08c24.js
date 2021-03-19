3d36580df1e70eb72a08214e87d48b4433674dcd
dipzcoded <dipoakerele@gmail.com>
2021-03-19 03:46:39 +0100

    done

---
+++ build/precache-manifest.9db745881903b74fdde6e0a53ff08c24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be0779b04466899b1eba5a7b57f9b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b833748302ddfcca45",
+    "url": "/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "901656fe3a428fc7de04",
+    "url": "/static/css/main.f6f73672.chunk.css"
+  },
+  {
+    "revision": "d4b833748302ddfcca45",
+    "url": "/static/js/2.01e38ade.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/static/js/2.01e38ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901656fe3a428fc7de04",
+    "url": "/static/js/main.02626af9.chunk.js"
+  },
+  {
+    "revision": "cb07d6deaf8f458e90ee",
+    "url": "/static/js/runtime-main.7b0dc955.js"
+  },
+  {
+    "revision": "0e600c1c16bae15fe60077a5ad96b14c",
+    "url": "/static/media/Sun-Tornado.0e600c1c.svg"
+  }
+]);
